36f1ce56605180cee23e31a397f343c3bf1b9a03
AICHHORN181 <=>
2020-02-17 10:35:11 +0100

    Implement maximum

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,5 +1,22 @@
 package com.codersbay;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        int iInt = 0;
+        String sString;
 
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Zahl: ");
+            sString = input.nextLine();
+            if (sString.charAt(0) == 'q')
+                break;
+            if (iInt < Integer.parseInt(sString))
+                iInt = Integer.parseInt(sString);
+        } while (true);
+
+        System.out.println(iInt);
+    }
 }
